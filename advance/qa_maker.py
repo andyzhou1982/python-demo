8f7a98b1286c49f8e3f14f8d40c0de5fc8d383a3
TCCN-zhoulingdi <andyzhou1982@163.com>
2023-12-22 11:14:29 +0800

    修改问答生成程序

--- advance/qa_maker.py
+++ advance/qa_maker.py
@@ -1,7 +1,7 @@
 
 # 读取txt文件
 import json
-from simbert.generate import gen_synonyms
+import random
 
 def read_file(path):
     with open(path, 'r') as f:
@@ -10,26 +10,29 @@ def read_file(path):
         contents = content.split('\n')
         #过滤数组中的空字符串
         contents = list(filter(lambda x: x!='', contents))
-        print(f"contents={contents}")
+        #print(f"contents={contents}")
+
+        #将text1:text2转换为json格式
+        def make_qa(question,answer):
+            output_dict = {
+                "question": question,
+                "answer": answer,
+                "history": []
+            }
+            for content in random.sample(contents, 5):
+                #构建一个元组
+                element = (content.split(':')[0],content.split(':')[1])
+                output_dict.get("history").append(element)
+            print(f"output_dict={output_dict}")
+            return output_dict
+        
         json_list = []
         for text in contents:
             split_str = text.split(':')
-            question = split_str[0]
-            answer = split_str[1]
-            json_list.append(make_qa(question,answer))
-            for gen_question in gen_synonyms(question,100,5):
-                json_list.append(make_qa(gen_question,answer))
-        print(f"json_list={json_list}")  
+            json_list.append(make_qa(split_str[0],split_str[1]))
+        print(f"json_list={json_list}")
     return json_list
 
-#将text1:text2转换为json格式
-def make_qa(question,answer):
-    output_dict = {
-        "question": question,
-        "answer": answer
-    }
-    #print(f"output_dict={output_dict}")
-    return output_dict
 
 def save_as_json(json_list, path):
     with open(path, 'w', encoding='utf-8') as f:
